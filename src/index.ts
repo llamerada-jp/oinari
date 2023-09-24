f6b22939222f5589bbd26ff80e758dbcaab587f4
Yuji Ito <llamerada.jp@gmail.com>
2023-09-24 16:18:56 +0900

    wip
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ import * as CM from "./command";
 import * as WB from "./webrtc_bypass_handler";
 import * as CRI from "./cri";
 import * as UI_AL from "./ui/app_loader";
+import * as UI_MAP from "./ui/map";
 import * as UI_MI from "./ui/migrate";
 import * as UI_PL from "./ui/proc_list";
 import * as UI_SI from "./ui/system_info";
@@ -66,6 +67,7 @@ async function initController(): Promise<void> {
 
 function initUI() {
   UI_AL.init(command);
+  UI_MAP.init();
   UI_MI.init(command);
   UI_PL.init(command);
 }
@@ -106,85 +108,3 @@ async function main(): Promise<void> {
 }
 
 main();
-
-/*
-import { Loader } from '@googlemaps/js-api-loader';
-import { AmbientLight, DirectionalLight, Scene } from "three";
-
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { ThreeJSOverlayView } from "@googlemaps/three";
-import { Keys } from "./keys";
-
-let map: google.maps.Map;
-
-const apiLoader = new Loader({
-  apiKey: Keys.googleApiKey,
-  version: "beta",
-});
-
-const mapOptions = {
-  tilt: 0,
-  heading: 0,
-  zoom: 18,
-  center: { lat: 35.6594945, lng: 139.6999859 },
-  mapId: Keys.googleMapId,
-  // disable interactions due to animation loop and moveCamera
-  disableDefaultUI: true,
-  gestureHandling: "none",
-  keyboardShortcuts: false,
-};
-
-apiLoader.load().then((google) => {
-  const mapDiv = document.getElementById("map") as HTMLElement;
-
-  map = new google.maps.Map(mapDiv, mapOptions);
-
-  const scene = new Scene();
-
-  const ambientLight = new AmbientLight(0xffffff, 0.75);
-
-  scene.add(ambientLight);
-
-  const directionalLight = new DirectionalLight(0xffffff, 0.25);
-
-  directionalLight.position.set(0, 10, 50);
-  scene.add(directionalLight);
-
-  // Load the model.
-  const loader = new GLTFLoader();
-  const url =
-    "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
-
-  loader.load(url, (gltf) => {
-    gltf.scene.scale.set(10, 10, 10);
-    gltf.scene.rotation.x = Math.PI / 2;
-    scene.add(gltf.scene);
-
-    let { tilt, heading, zoom } = mapOptions;
-
-    const animate = () => {
-      if (tilt < 67.5) {
-        tilt += 0.5;
-      } else if (heading <= 360) {
-        heading += 0.2;
-        zoom -= 0.0005;
-      } else {
-        // exit animation loop
-        return;
-      }
-
-      map.moveCamera({ tilt, heading, zoom });
-
-      requestAnimationFrame(animate);
-    };
-
-    requestAnimationFrame(animate);
-  });
-
-  new ThreeJSOverlayView({
-    map,
-    scene,
-    anchor: { ...mapOptions.center, altitude: 100 },
-  });
-});
-//*/
